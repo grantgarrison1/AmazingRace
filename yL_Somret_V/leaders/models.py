6da588d5e02aa54c29b39ac28dd77e9d15070370
Blank1416 <109182759+Blank1416@users.noreply.github.com>
2025-02-21 16:34:00 -1000

    Delete yL_Somret_V directory

--- yL_Somret_V/leaders/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class leader(AbstractUser):
-    phone = models.CharField(max_length = 10)
-
-    def __str__(self):
-        return self.username
-# Create your models here.
